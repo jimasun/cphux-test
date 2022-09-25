6386ee0acbffb8c8bdc2c19fd1dac6c8540b2ec8
jimasun <marius.jigoreanu@gmail.com>
2022-09-25 20:53:41 +0200

    improved the csv table

--- resources/views/livewire/csv.blade.php
+++ resources/views/livewire/csv.blade.php
@@ -1,17 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
-<div class="content">
+@section('title', 'Salary Survey')
 
-    @section('title', 'Salary Survey')
+@section('content')
 
-    @section('content')
+    <div class="w-full mx-auto p-4">
 
-        <div class="container mx-auto px-4 text-sm leading-tight">
-            @foreach ($entries as $entry)
-                @include('livewire.includes.csvRow', ['row' => $entry])
-            @endforeach
+        @include('livewire.includes.dropdown', [
+            'yoeColIdx' => $yoeColIdx,
+            'entries' => $entries,
+            'dropdownText' => $dropdownText,
+            'dropdownItems' => $dropdownItems
+        ])
+
+    </div>
+
+    <div class="table table-fixed w-full mx-auto px-4 text-sm leading-tight">
+
+        <div class="table-header-group">
+            <div class="table-row max-h-2 font-semibold">
+                @foreach ($entries->current() as $cell)
+                    <div class="table-cell p-2">
+                        {{ $cell }}
+                    </div>
+                @endforeach
+                {{ $entries->next() }}
+            </div>
         </div>
 
-    @endsection
+        <div class="table-row-group">
+            @while ($entries->valid())
+                <div id="row-{{$entries->key()}}" class="table-row hover:bg-gray-200">
+                    @foreach ($entries->current() as $cell)
+                        <div class="table-cell max-h-min p-2 overflow-hidden hover:font-semibold hover:overflow-visible hover:drop-shadow-xl">
+                            {{ $cell }}
+                        </div>
+                    @endforeach
+                    {{ $entries->next() }}
+                </div>
+            @endwhile
+        </div>
 
-</div>
+    </div>
+
+@endsection
